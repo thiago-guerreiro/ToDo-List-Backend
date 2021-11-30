fbe93e6f517b06b848098448ffb36da89b3bca5c
Thiago Guerreiro <thiagoguerreiro.si@gmail.com>
2021-11-30 16:31:10 -0300

    feat: método findAllOpen

--- src/main/java/com/thiago/todo/repositories/TodoRepository.java
+++ src/main/java/com/thiago/todo/repositories/TodoRepository.java
@@ -1,11 +1,17 @@
 package com.thiago.todo.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.thiago.todo.domain.Todo;
 
 @Repository
 public interface TodoRepository extends JpaRepository<Todo, Long> {
+	
+	@Query("SELECT obj FROM Todo obj WHERE obj.finalizado = false ORDER BY obj.dataParaFinalizar ASC")
+	List<Todo> findAllOpen();
 
 }
--- src/main/java/com/thiago/todo/services/TodoService.java
+++ src/main/java/com/thiago/todo/services/TodoService.java
@@ -1,5 +1,6 @@
 package com.thiago.todo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +18,11 @@ public class TodoService {
 	public Todo findById(Long id) {
 		Optional<Todo> obj = repository.findById(id);
 		return obj.orElse(null);
+	}
+	
+	public List<Todo> findAllOpen() {
+		List<Todo> list = repository.findAllOpen();
+		return list;
 	}
 
 }
